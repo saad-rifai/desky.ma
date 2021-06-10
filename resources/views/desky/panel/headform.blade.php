c9ea84f6a4bfd37e9eace601fd1641ebf1a08f90
saad rifai <rifaisaad3@gmail.com>
2021-06-10 17:35:51 +0100

    GOOGLE ANAl

--- resources/views/desky/panel/headform.blade.php
+++ resources/views/desky/panel/headform.blade.php
@@ -56,7 +56,15 @@
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.8.2/css/all.css"
             integrity="sha384-xVVam1KS4+Qt2OrFa+VdRUoXygyKIuNWUUUBZYv+n27STsJ7oDOHJgfF0bNKLMJF" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S1XT2DT4WF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-S1XT2DT4WF');
+</script>
     </head>
 @php
 $datajson = file_get_contents('database/data.json');
