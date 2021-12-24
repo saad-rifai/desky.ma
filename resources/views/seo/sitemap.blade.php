a977900acc04b4dd34310ecbbac3721ab865f510
saad rifai <rifaisaad3@gmail.com>
2021-12-24 21:10:23 +0100

    sitemap

--- resources/views/seo/sitemap.blade.php
+++ resources/views/seo/sitemap.blade.php
@@ -1,10 +1,10 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
         <loc>{{asset('/sitemap-1.xml')}}</loc>
       </sitemap>
       <sitemap>
         <loc>{{asset('/sitemap-2.xml')}}</loc>
       </sitemap>
 
-</urlset>
+</sitemapindex>
